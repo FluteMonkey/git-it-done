1fdb73b9a6654e2be7851271e952f3299048de12
Zchun <z.c.chun2003@gmail.com>
2022-05-31 18:26:41 -0500

    Created a submission form

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,30 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
 var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
+    //make a get request to github
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
     });
 };
-  
-  getUserRepos("microsoft");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    //get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        //clears input field
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+};
+  userFormEl.addEventListener("submit", formSubmitHandler);
