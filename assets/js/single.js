0d966f6bafa6a163c32550f1f957edc4cf14f00d
Zchun <z.c.chun2003@gmail.com>
2022-06-02 18:42:14 -0500

    fixed code to account for possible errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -6,8 +6,12 @@ var getRepoName = function() {
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
 
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
+    if(repoName) {
+        getRepoIssues(repoName);
+        repoNameEl.textContent = repoName;
+    } else {
+        document.location.replace("./index.html");
+      }
 }
 
 var getRepoIssues = function(repo) {
@@ -24,7 +28,7 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 
